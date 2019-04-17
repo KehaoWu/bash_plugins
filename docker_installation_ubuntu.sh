752d8e4d27ed979737512a424e8a11e03eb8c106
KeHao Wu <kehao.wu@gmail.com>
2019-04-17 10:53:29 +0800

    Update docker_installation_ubuntu.sh

--- docker_installation_ubuntu.sh
+++ docker_installation_ubuntu.sh
@@ -6,11 +6,15 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get install \
 apt-transport-https \
-ca-certificates \
-curl \
-gnupg-agent \
-software-properties-common
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+       
 sudo apt-key fingerprint 0EBFCD88
+    
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 7EA0A9C3F273FCD8 
+
     
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
